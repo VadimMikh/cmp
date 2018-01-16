5bbc4a8b85e66200b3946c74c05e2e2d702c1dd5
VadimMikh <vadossm82@gmail.com>
2018-01-16 12:02:59 +0100

    Version 2.0.0

--- js/main.js
+++ js/main.js
@@ -15,7 +15,6 @@ function app() {
             if (elem.nodeType !== Node.ELEMENT_NODE) {
                 continue;
             }
-
             if (!ishaveselector || elem.matches(selector)) {
                 elements.push(elem);
             }
@@ -30,7 +29,14 @@ function app() {
 
     function navigate(e) {
         e.preventDefault();
-        body.classList.contains('chat_active') ? body.classList.remove('chat_active') : body.classList.add('chat_active');
+        removeEvent();
+        if (body.classList.contains('chat_active')) {
+            body.classList.remove('chat_active');
+            getPage('profile.html');
+        } else {
+            body.classList.add('chat_active');
+            getPage('chat.html');
+        }
     }
 
     function like(e) {
@@ -42,27 +48,32 @@ function app() {
         item.addEventListener('click', navigate);
     });
 
+    function removeEvent() {
+        navigateBut.forEach(function (item) {
+            item.removeEventListener('click', navigate);
+        });
+    }
+
     likeBut.forEach(function (item) {
         item.addEventListener('click', like);
     });
 
     !!addFriend && addFriend.addEventListener('click', friend);
 
-    function Message (text) {
-        this.text = text;
+    function Message () {
         this.div = document.createElement('div');
         this.imgLink = document.createElement('a');
         this.imgWrap = document.createElement('a');
         this.img = document.createElement('img');
         this.textBox = document.createElement('div');
-
-        return this.write();
+        this.messages = [];;
     }
 
     Message.prototype = {
         constructor: Message,
 
-        write: function write() {
+        write: function write(text) {
+            var m = text;
             this.div.classList.add('message');
             this.imgLink.classList.add('message-user_image');
             this.img.src = 'img/boy.jpg';
@@ -72,20 +83,44 @@ function app() {
             this.imgLink.appendChild(this.img);
             this.imgWrap.appendChild(this.imgLink);
             this.div.appendChild(this.imgWrap);
-            this.textBox.innerHTML = this.text;
+            this.textBox.innerHTML = m;
             this.div.appendChild(this.textBox);
 
             return this.div;
+        },
+
+        add: function add(message) {
+            saveMessage(message);
+        }
+    };
+
+    var messageList = [];
+    function saveMessage(text) {
+        messageList.push(text);
+        var store = JSON.stringify(messageList);
+        sessionStorage.setItem('messages', store);
+    }
+    function getMessages() {
+        var store = sessionStorage.getItem('messages');
+        if (store) {
+            messageList = JSON.parse(store);
+            messageList.forEach(function (item) {
+                var m = new Message();
+                !!chatBox && chatBox.appendChild(m.write(item));
+            });
         }
     }
+    getMessages();
 
     function addMessage(e) {
         if (e.keyCode === 13) {
             e.preventDefault();
             var v = messageInput.value,
-                m = new Message(v);
+                m = new Message(),
+                res = m.write(v);
 
-            v !== '' && chatBox.appendChild(m);
+            m.add(v);
+            v !== '' && chatBox.appendChild(res);
 
             messageInput.value = '';
         }
@@ -93,4 +128,29 @@ function app() {
     !!messageInput && messageInput.addEventListener('keydown', addMessage);
 
 }
-window.addEventListener('load', app);
+
+function getPage(url) {
+    var xhr = new XMLHttpRequest(),
+        enterPoint = document.getElementById('main');
+
+    xhr.open('GET', url, true);
+
+    xhr.send();
+
+    return xhr.onreadystatechange = function() {
+        if (xhr.readyState != 4) return;
+
+        if (xhr.status != 200) {
+            enterPoint.innerHTML = '<div class="not_found">Page not found</div>'
+        } else {
+            enterPoint.innerHTML = xhr.responseText;
+            app();
+        }
+
+    };
+    enterPoint.innerHTML = 'Loading...';
+}
+function init() {
+    getPage('profile.html');
+}
+window.addEventListener('load', init);
